928df55b461cba36a4839103e11747fd9272c1cb
lalalens <lalagilbert@shelontas-mbp.myfiosgateway.com>
2023-07-17 00:28:26 -0400

    Completed tile layers

---
+++ static/leaflet-Part-1/js copy/logic.js
@@ -0,0 +1,66 @@
+function createMap(earthquakeLayer) {
+    let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    let baseMaps = {
+        "Street Map": streetMap
+    };
+
+    let overlayMaps = {
+        "Earthquakes": earthquakeLayer
+    };
+
+    let map = L.map("map", {
+        center: [37.0902, -95.7129],
+        zoom: 5,
+        layers: [streetMap, earthquakeLayer]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+}
+
+function createMarkers(response) {
+    let earthquakes = response.features;
+    let quakeMarkers = [];
+
+    for (let i = 0; i < earthquakes.length; i++) {
+        let earthquake = earthquakes[i];
+        let magnitude = earthquake.properties.mag;
+        let depth = earthquake.geometry.coordinates[2];
+        let lat = earthquake.geometry.coordinates[1];
+        let lon = earthquake.geometry.coordinates[0];
+        let quakeMarker = L.circle([lat, lon], {
+            fillOpacity: 0.75,
+            color: getColor(depth),
+            fillColor: getColor(depth),
+            radius: magnitude * 20000
+        }).bindPopup("<h3>Magnitude: " + magnitude + "<h3><h3>Depth: " + depth + "</h3>");
+
+        quakeMarkers.push(quakeMarker);
+    }
+
+    createMap(L.layerGroup(quakeMarkers));
+}
+
+// Function to determine marker color based on earthquake depth
+function getColor(depth) {
+    if (depth > 90) {
+        return "red";
+    } else if (depth > 70) {
+        return "darkorange";
+    } else if (depth > 50) {
+        return "orange";
+    } else if (depth > 30) {
+        return "gold";
+    } else if (depth > 10) {
+        return "yellow";
+    } else {
+        return "lightgreen";
+    }
+}
+
+// Perform an API call to the USGS API to get the earthquake information. Call createMarkers when it completes.
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(createMarkers);
